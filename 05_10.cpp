5e4a5f43544056b74f0a9773b83e5f229a237002
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-10-12 22:15:18 +0600

    Incorporated best practices

--- 05_10.cpp
+++ 05_10.cpp
@@ -1,143 +1,122 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 class Fraction {
 private:
   int numerator;
   int denominator;
 
-public:
-  string zero = "\nDividing by zero is like cutting the cake into 0 pieces, "
-                "which doesn't make any sense at all if you think of it, as "
-                "the cake itself is already made of a single piece\n\n";
+  static const std::string ZERO_DIVISION_ERROR;
 
+public:
   Fraction() : numerator(0), denominator(1) {}
 
   Fraction(int nmrtr, int dnmntr) : numerator(nmrtr), denominator(dnmntr) {
     if (denominator == 0) {
-      cout << zero;
+      std::cout << ZERO_DIVISION_ERROR;
       denominator = 1;
     }
   }
 
   void edit() {
-    cout << "Enter numerator: ";
-    cin >> numerator;
-    cout << "Enter denominator: ";
-    cin >> denominator;
-    if (denominator == 0) {
-      cout << zero;
+    std::cout << "Enter numerator: ";
+    std::cin >> numerator;
+    std::cout << "Enter denominator: ";
+    std::cin >> denominator;
+
+    if (std::cin.fail()) {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Invalid input. Setting to default values.\n";
+      numerator = 0;
+      denominator = 1;
+    } else if (denominator == 0) {
+      std::cout << ZERO_DIVISION_ERROR;
       denominator = 1;
     }
   }
 
-  void print() {
-    cout << "Fraction: " << numerator << "/" << denominator << "\n";
+  void print() const {
+    std::cout << "Fraction: " << numerator << "/" << denominator << "\n";
   }
 
   Fraction add(const Fraction &other) const {
-    Fraction result;
-    if (denominator == other.denominator) {
-      result.numerator = numerator + other.numerator;
-      result.denominator = denominator;
-    } else {
-      result.numerator =
-          (numerator * other.denominator) + (other.numerator * denominator);
-      result.denominator = denominator * other.denominator;
-    }
-    return result;
+    return Fraction(numerator * other.denominator +
+                        other.numerator * denominator,
+                    denominator * other.denominator);
   }
 
   Fraction subtract(const Fraction &other) const {
-    Fraction result;
-    if (denominator == other.denominator) {
-      result.numerator = numerator - other.numerator;
-      result.denominator = denominator;
-    } else {
-      result.numerator =
-          (numerator * other.denominator) - (other.numerator * denominator);
-      result.denominator = denominator * other.denominator;
-    }
-    return result;
+    return Fraction(numerator * other.denominator -
+                        other.numerator * denominator,
+                    denominator * other.denominator);
   }
 
   Fraction multiply(const Fraction &other) const {
-    Fraction result;
-    if (denominator == other.denominator) {
-      result.numerator = numerator * other.numerator;
-      result.denominator = denominator;
-    } else {
-      result.numerator = numerator * other.numerator;
-      result.denominator = denominator * other.denominator;
-    }
-    return result;
+    return Fraction(numerator * other.numerator,
+                    denominator * other.denominator);
   }
 
   Fraction divide(const Fraction &other) const {
-    Fraction result;
-    result.numerator = numerator * other.denominator;
-    result.denominator = denominator * other.numerator;
-    if (result.denominator == 0) {
-      cout << zero;
+    if (other.numerator == 0) {
+      std::cout << ZERO_DIVISION_ERROR;
+      return Fraction();
     }
-    return result;
+    return Fraction(numerator * other.denominator,
+                    denominator * other.numerator);
   }
-
-  Fraction operator+(const Fraction &other) const { return add(other); }
-
-  Fraction operator-(const Fraction &other) const { return subtract(other); }
-
-  Fraction operator*(const Fraction &other) const { return multiply(other); }
-
-  Fraction operator/(const Fraction &other) const { return divide(other); }
 };
 
-void menu() {
-  Fraction x;
-  Fraction y;
-  Fraction result;
-  int choice;
-  bool stop = false;
+const std::string Fraction::ZERO_DIVISION_ERROR =
+    "\nDividing by zero is like cutting the cake into 0 pieces, which doesn't "
+    "make any sense at all.\n";
 
-  while (!stop) {
-    cout << "\nSet first fraction:\n";
+int main() {
+  Fraction x, y, result;
+  int choice;
+
+  while (true) {
+    std::cout << "\nSet first fraction:\n";
     x.edit();
     x.print();
 
-    cout << "\nSet second fraction:\n";
+    std::cout << "\nSet second fraction:\n";
     y.edit();
     y.print();
 
-    cout << "\nFraction operations:\n1) Add\n2) Substract\n3) Multiply\n4) "
-            "Divide\n0) Exit\nChoose operation: ";
-    cin >> choice;
+    std::cout << "\nFraction operations:\n1) Add\n2) Subtract\n3) Multiply\n4) "
+                 "Divide\n0) Exit\nChoose operation: ";
+    std::cin >> choice;
+
+    if (std::cin.fail()) {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Invalid choice. Try again.\n";
+      continue;
+    }
 
     switch (choice) {
     case 1:
-      result = x + y;
+      result = x.add(y);
       result.print();
       break;
     case 2:
-      result = x - y;
+      result = x.subtract(y);
       result.print();
       break;
     case 3:
-      result = x * y;
+      result = x.multiply(y);
       result.print();
       break;
     case 4:
-      result = x / y;
+      result = x.divide(y);
       result.print();
       break;
     case 0:
-      stop = true;
-      break;
+      return 0;
     default:
-      cout << "Wrong input\n";
+      std::cout << "Wrong input\n";
       break;
     }
   }
 }
-
-int main() { menu(); }
